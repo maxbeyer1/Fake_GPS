7507e4b43312ed530e87c1aa6a9e22bc74b5373e
Max Beyer <max.beyer09@gmail.com>
2025-11-19 18:13:33 -0600

    Initial Commit

---
+++ Fake_GPS/Fake_GPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fake_GPSApp.swift
+//  Fake_GPS
+//
+//  Created by Max Beyer on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Fake_GPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
